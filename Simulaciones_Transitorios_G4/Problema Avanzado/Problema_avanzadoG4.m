1a87688dd195e1f22333dfa3f0bb534f0a67df5c
srt775 <srt775@inlumine.ual.es>
2025-10-20 09:18:04 +0200

    Delete Simulaciones_Transitorios_G4 directory

--- Simulaciones_Transitorios_G4/Problema Avanzado/Problema_avanzadoG4.m
+++
@@ -1,8 +0,0 @@
-sim("Problema_Avanzado.slx")
-
-figure(1)
-plot(Intensidad_L,'LineWidth', 2)
-title('Evolución temporal de la corriente de la inductancia')
-xlabel('Tiempo (s)')
-ylabel('Amplitud (A)')
-
