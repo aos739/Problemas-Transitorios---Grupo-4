4452859ecf3bda61ae75365c70f6dcc6334f53d2
srt775 <srt775@inlumine.ual.es>
2025-10-20 21:14:03 +0200

    Add files via upload

---
+++ Simulaciones_Transitorios_G4/Problema Avanzado/Problema_avanzadoG4.m
@@ -0,0 +1,8 @@
+sim("Problema_Avanzado.slx")
+
+figure(1)
+plot(Intensidad_L,'LineWidth', 2)
+title('Evolución temporal de la corriente de la inductancia')
+xlabel('Tiempo (s)')
+ylabel('Intensidad (A)')
+
